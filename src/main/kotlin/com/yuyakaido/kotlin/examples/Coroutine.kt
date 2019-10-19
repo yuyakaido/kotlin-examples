eaf7ecb44777c569ef56d21f74c31dc12d504133
Yuya Kaido <yuyakaido@gmail.com>
2019-10-19 21:39:36 +0900

    Add some examples for coroutine

--- src/main/kotlin/com/yuyakaido/kotlin/examples/Coroutine.kt
+++ src/main/kotlin/com/yuyakaido/kotlin/examples/Coroutine.kt
@@ -3,15 +3,53 @@ package com.yuyakaido.kotlin.examples
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
-    println("Start")
+    runCancellationExample()
+}
 
+private fun runGlobalScopeExample() {
     GlobalScope.launch {
         delay(1000)
-        println("Hello")
+        println("World")
     }
-
+    println("Hello")
     Thread.sleep(2000)
-    println("Stop")
+}
+
+private fun runRunBlockingExample() {
+    runBlocking {
+        GlobalScope.launch {
+            delay(1000)
+            println("World")
+        }
+        println("Hello")
+        delay(2000)
+    }
+}
+
+private fun runCoroutineScopeExample() {
+    runBlocking {
+        launch {
+            delay(1000)
+            println("World")
+        }
+        println("Hello")
+        delay(2000)
+    }
+}
+
+private fun runCancellationExample() {
+    runBlocking {
+        val job = launch {
+            while (true) {
+                delay(1000)
+                println("World")
+            }
+        }
+        println("Hello")
+        delay(3000)
+        job.cancel()
+    }
 }
